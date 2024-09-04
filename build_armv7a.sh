31ecd19e43536d6f5054926fdc8437121e9d9c97
frank <feixiao2020@sina.com>
2024-09-04 15:44:47 +0800

    添加脚本执行编译

---
+++ build_armv7a.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# OSX
+#NDK=$HOME/Library/Android/sdk/ndk/21.1.6352462
+#TOOLCHAIN=$NDK/toolchains/llvm/prebuilt/darwin-x86_64
+
+echo $(pwd)
+NDK=/home/frank/android/ndk/android-ndk-r20b
+TOOLCHAIN=$NDK/toolchains/llvm/prebuilt/linux-x86_64
+
+OUT_DIR=$(pwd)/out/ffmpeg-6.1
+
+
+cd ./ffmpeg-6.1
+echo $(pwd)
+
+ARCH=armv7a
+CPU=armv7a
+API=21
+CC=$TOOLCHAIN/bin/armv7a-linux-androideabi$API-clang
+CXX=$TOOLCHAIN/bin/armv7a-linux-androideabi$API-clang++
+SYSROOT=$NDK/toolchains/llvm/prebuilt/linux-x86_64/sysroot
+CROSS_PREFIX=$TOOLCHAIN/bin/arm-linux-androideabi-
+PREFIX=$OUT_DIR/armv7a
+OPTIMIZE_CFLAGS="-march=$CPU"
+
+./configure \
+	--prefix=$PREFIX --disable-postproc \
+	--disable-debug --disable-doc \
+	--disable-symver --disable-doc --disable-avdevice \
+	--enable-gpl --enable-static  \
+	--enable-neon --enable-hwaccels --enable-jni \
+	--enable-small --enable-mediacodec \
+	--cross-prefix=$CROSS_PREFIX --target-os=android \
+	--arch=$ARCH --cpu=$CPU \
+	--cc=$CC  --cxx=$CXX \
+	--enable-cross-compile \
+	--sysroot=$SYSROOT \
+	--extra-cflags="-Os -fpic $OPTIMIZE_CFLAGS" \
+	--extra-ldflags="-L $ADDI_LDFLAGS" \
+	--pkg-config="pkg-config --static"
+
+make -j 8
+make install
---
+++ ubuntu.md
@@ -0,0 +1,13 @@
+### Ubuntu
+
+#### 下载代码
+```shell
+wget https://ffmpeg.org/releases/ffmpeg-6.1.tar.gz
+
+tar -zxvf ffmpeg-6.1.tar.gz
+```
+
+#### 执行脚本
+```shell
+./build_armv7a.sh
+```
